216cf9f71e5646e6698eb7ed82cd478ed46de3a1
David Muramatsu <D.Muramatsu@physik.uni-muenchen.de>
2023-11-03 15:10:49 +0100

    wrote convert_csv_to_h5.py. get 'No module named 'comsol_to_h5'' error. Need to fix this.

--- convert_csv_to_h5.py
+++ convert_csv_to_h5.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     argv = sys.argv[1:]
 
     try:
-         opts, args = getopt.getopt(argv,"hi:o:p:d:f:",["input-file=","output-file=", 
+         opts, args = getopt.getopt(argv,"hi:o:p:d:f:",["input-file=","output-file=",
                                                       "structured=", "parameter-file=", 
                                                       "description-line-number=",
                                                       "format="])
@@ -17,11 +17,11 @@ if __name__ == "__main__":
               '-p <parameterfile> -d <line number of description line>' +
               '-f <file format (csv or columnar, defaults to csv)>')
         sys.exit(2)
-        
+
     INPUT_FILE_NAME = None
     OUTPUT_FILE_NAME = None
     PARAMETER_FILE_NAME = None
-    HEADER_LINE_NUMBER = 7
+    HEADER_LINE_NUMBER = -1
     FILE_FORMAT = 'csv'
 
     for opt, arg in opts:
@@ -40,7 +40,8 @@ if __name__ == "__main__":
             HEADER_LINE_NUMBER = int(arg)
         elif opt in ("-f", "--format"):
             FILE_FORMAT = arg
-            
+
+    # check right specification of file format
     if FILE_FORMAT not in ('csv', 'columnar'):
         raise RuntimeError('file format specified was \'' + FILE_FORMAT + '\', ' +
                            'valid formats are \';\'-separated csv and columnar.')
@@ -52,19 +53,15 @@ if __name__ == "__main__":
         FILE_DF = comsol_to_h5.read_from_comsol_native_csv(INPUT_FILE_NAME)
     elif FILE_FORMAT == 'columnar':
         with tempfile.TemporaryDirectory() as TEMP_DIR:
-            
-    #write a cleaned file, red in and rm cleaned file
-    cleaned_file_name = OUTPUT_FILE_NAME + ".cleaned_file"
-    clean_file(INPUT_FILE_NAME, cleaned_file_name, column_description_line= HEADER_LINE_NUMBER)
-    source_df = pd.read_csv(cleaned_file_name, comment='%', delimiter=';')
-    os.popen("rm " + cleaned_file_name)
+            TEMP_FILE_NAME = TEMP_DIR + "/cleaned.txt"
+            comsol_to_h5.clean_file(INPUT_FILE_NAME, TEMP_FILE_NAME, HEADER_LINE_NUMBER)
+            FILE_DF = comsol_to_h5.read_from_comsol_native_csv(TEMP_FILE_NAME)
 
 
     STATIC_PARAMETER_DICT = None
     if PARAMETER_FILE_NAME is not None:
         STATIC_PARAMETER_DICT = comsol_to_h5.extract_parameters_static(PARAMETER_FILE_NAME)
-        comsol_to_h5.write_data_to_h5(source_df, OUTPUT_FILE_NAME, 
-                                      parameters_static=STATIC_PARAMETER_DICT)
 
     else:
-        comsol_to_h5.write_data_to_h5(source_df, OUTPUT_FILE_NAME)
+        comsol_to_h5.write_data_to_h5(FILE_DF, OUTPUT_FILE_NAME, 
+                                      parameters_static=STATIC_PARAMETER_DICT)
